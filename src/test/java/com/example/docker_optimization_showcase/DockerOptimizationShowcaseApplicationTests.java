e22be856c5a798d42b83ee6b149303f9dd9fcffc
Leonardo Cottino <leonardo.cottino@hotmail.it>
2026-07-07 18:19:01 +0200

    Initial commit

---
+++ src/test/java/com/example/docker_optimization_showcase/DockerOptimizationShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker_optimization_showcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerOptimizationShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
